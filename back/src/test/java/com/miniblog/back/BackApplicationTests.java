e9bdf5ae291b52ef310fd51442ea483aa6e4573d
ghtjr410 <ghtjr410@gmail.com>
2025-01-10 23:46:00 +0900

    back - pom.xml 의존성 업데이트   - JWT 관련 의존성 추가 (`jjwt-api`, `jjwt-impl`, `jjwt-jackson`).   - MySQL 드라이버 추가 (`mysql-connector-j`).   - Lombok 및 OpenAPI 문서화 도구 추가 (`lombok`, `springdoc-openapi-starter-webmvc-ui`).
    - application.properties
      - 데이터베이스 URL, 사용자명, 비밀번호 설정 추가
      - JWT 시크릿 키 및 토큰 만료 시간 설정 추가

---
+++ back/src/test/java/com/miniblog/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniblog.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
